83956917404ce6e10a3fd8ccec7b64190c54e043
XinAnzzZ <13023195022@163.com>
2018-09-12 14:48:20 +0800

    xx

--- src/main/java/com/yuhangma/controller/TestController.java
+++ src/main/java/com/yuhangma/controller/TestController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class TestController {
 
-    @GetMapping("/")
+    @GetMapping(value={"/", ""})
     public String test() {
 
         return "test";
