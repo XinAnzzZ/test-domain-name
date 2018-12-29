6dbab9aba586b22eee3c8ad592a5f68538e0ff93
XinAnzzZ <13023195022@163.com>
2018-12-29 11:20:03 +0800

    test

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-cd /docker/java-service/test-domain-name/test-domain-name
-git pull -u origin master
+# 此脚本用于重新部署本服务
+
+# 进入到项目目录根目录下
 cd /docker/java-service/test-domain-name
+
+# 拉取最新的代码
+git pull origin master
+
+# 重启 docker compose
 docker-compose restart
